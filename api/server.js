31223dc2d592a7ed350dd97df08e389b21e88245
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-05-17 17:23:41 +0300

    get kısmı yapıldı.

--- api/server.js
+++ api/server.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const server = express();
 const usersRouter = require("./users/users-router");
-const middleWare = require("./middleware/middleware");
+const { logger } = require("./middleware/middleware");
 
 // ekspres'in varsayılan olarak istek gövdelerinde JSON'u ayrıştıramayacağını unutmayın
 server.use(express.json());
 
 // global ara yazılımlar ve kullanıcı routelarının buraya bağlanması gerekir
 server.use(logger);
-server.use("./api/users", usersRouter);
+server.use("/api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Biraz ara yazılım yazalım!</h2>`);
+});
+
+server.use((err, req, res, next) => {
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Server error!.." });
 });
 
 module.exports = server;
