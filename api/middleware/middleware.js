f449cdffadf0dcfb977dfed5830f0f5fe656f077
Aysenagay <101216489+Aysenagay@users.noreply.github.com>
2023-05-17 16:28:05 +0300

    middleware tamamlandı.

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,47 @@
+const Users = require("../users/users-model");
+
 function logger(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  console.log(`${new Date().toISOString()} ${req.method} to ${req.url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  const user = Users.getById(req.params.id);
+  if (!user) {
+    next({ status: 404, message: `${id}'li user yok!...` });
+  } else {
+    next();
+  }
 }
 
 function validateUser(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).json({ message: "gerekli name alanı eksik" });
+  } else {
+    req.name = name;
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
   // SİHRİNİZİ GÖRELİM
+  const { text } = req.body;
+  if (!text) {
+    res.status(400).json({ message: "gerekli text alanı eksik" });
+  } else {
+    req.text = text;
+    next();
+  }
 }
 
 // bu işlevleri diğer modüllere değdirmeyi unutmayın
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost,
+};
--- api/server.js
+++ api/server.js
@@ -1,10 +1,14 @@
 const express = require("express");
-
 const server = express();
+const usersRouter = require("./users/users-router");
+const middleWare = require("./middleware/middleware");
 
 // ekspres'in varsayılan olarak istek gövdelerinde JSON'u ayrıştıramayacağını unutmayın
+server.use(express.json());
 
 // global ara yazılımlar ve kullanıcı routelarının buraya bağlanması gerekir
+server.use(logger);
+server.use("./api/users", usersRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Biraz ara yazılım yazalım!</h2>`);
